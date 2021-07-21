c36466cee2bb30d607167b56268b62f752a7bec3
Bruno Yamada <bfyamada@outlook.com>
2021-07-20 21:44:35 -0300

    Adicionando home e classe Ofertas

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertaService } from '../oferta.service';
+import { Oferta } from '../shared/oferta.model';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertaService]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas : Oferta[] = []
+
+  constructor(private ofertaService: OfertaService) { }
+
+  ngOnInit(): void {
+    this.ofertas = this.ofertaService.getOferta()
+    console.log(this.ofertas)
+  }
+
+}
