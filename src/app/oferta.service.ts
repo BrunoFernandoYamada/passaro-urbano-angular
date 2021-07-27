0788604f91dac9b516261f381676874816c54814
Bruno Yamada <bfyamada@outlook.com>
2021-07-26 22:09:29 -0300

    Observbles, retry and map

--- src/app/oferta.service.ts
+++ src/app/oferta.service.ts
@@ -2,6 +2,9 @@ import { Oferta } from "./shared/oferta.model";
 import { HttpClient} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from 'src/environments/environment';
+import { Observable } from "rxjs";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retry'
 
 @Injectable()
 export class OfertaService {
@@ -51,6 +54,15 @@ export class OfertaService {
         .then((resposta : any) => {
             return resposta[0].descricao
         })
+
+    }
+
+    public pesquisaOfertas(termo : string) : Observable<Oferta[]> {
+
+        return this.http.get(`${environment.URL_API}/ofertas?descricao_oferta_like=${termo}`)
+        .retry(10)
+        .map((resposta : any) =>  resposta
+        )
 
     }
 
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OfertaService } from '../oferta.service';
+import { Oferta } from '../shared/oferta.model';
+
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertaService]
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas? : Observable<Oferta[]>
+
+  constructor(private ofertaService : OfertaService) { }
 
   ngOnInit(): void {
+    
+  }
+
+  pesquisa(termodaBusca: string): void {
+    this.ofertas = this.ofertaService.pesquisaOfertas(termodaBusca)
+
+    this.ofertas.subscribe(
+      (data: Oferta[]) => console.log(data),
+      (erro: any) => console.log('Status do Erro: ', erro.status)
+    )
+      
   }
 
 }
